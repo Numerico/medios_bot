2bc705c57d114d9c6b9559d568b35210622e7716
Roberto Soto <roberto@codigosur.org>
2017-10-10 01:50:34 -0500

    al comando /monitor contesta con boton de posicion como manejar la respuesta del boton?

--- medios_libres/medios_libres/management/commands/bot_listen.py
+++ medios_libres/medios_libres/management/commands/bot_listen.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from medios_libres.models import Texto
 import telebot
+from telebot import types
 
 # token medios libres
 token = "317988596:AAEH2w1RxDYg26hf0aVVFuqtscM7d8dr98Q"
@@ -10,9 +11,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         bot = telebot.TeleBot(token)
-        def handle_messages(messages):
-            for message in messages:
-                Texto.objects.create(texto=message.text)
-        bot.set_update_listener(handle_messages)
-        bot.polling()
 
+        mrkp = types.ReplyKeyboardMarkup(row_width=1)
+        boton = types.KeyboardButton('Enviar posicion', request_location=True)
+        mrkp.add(boton)
+ 
+        @bot.message_handler(commands=['monitor',])
+        def query_text(message):
+            cid = message.chat.id
+            bot.send_message(cid, "Reportar mi ubicacion", reply_markup=mrkp)
+
+        bot.polling()
