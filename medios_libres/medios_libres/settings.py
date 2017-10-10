5bb5d7e247b6f77ceb4953e7a1c6b073b5878024
Roberto Soto <roberto@codigosur.org>
2017-10-10 03:06:17 -0500

    bd sqlite para desarrollo

--- medios_libres/medios_libres/settings.py
+++ medios_libres/medios_libres/settings.py
@@ -74,17 +74,22 @@ WSGI_APPLICATION = 'medios_libres.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgres',
+#        'USER': 'postgres',
+#        'PASSWORD': 'laSextaDeclaracion',
+#        'HOST': 'db',
+#        'PORT': 5432
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'laSextaDeclaracion',
-        'HOST': 'db',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'site.db',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
