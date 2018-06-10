89a37e134b3566b2927fd7311fccffd17eb2db5e
Roberto Soto <webmaster@numerica.cl>
2018-06-09 22:30:31 -0700

    bot as model

--- medios_libres/medios_libres/management/commands/bot_listen.py
+++ medios_libres/medios_libres/management/commands/bot_listen.py
@@ -1,42 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
-from medios_libres.models import Texto, Ubicacion
-import telebot
-from telebot import types
-
-# token medios libres
-token = "317988596:AAEH2w1RxDYg26hf0aVVFuqtscM7d8dr98Q"
+from medios_libres.models import MediosBot
 
 class Command(BaseCommand):
-    help = 'Listen to Telegram Bot in the background'
+    help = """Listen to Telegram Bot in the background
+              (medios_venv)$ python manage.py bot_listen"""
 
     def handle(self, *args, **options):
-        bot = telebot.TeleBot(token)
-
-        geo = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-        boton = types.KeyboardButton('Enviar posicion', request_location=True)
-	geo.add(boton)
- 
-        @bot.message_handler(commands=['monitor',])
-        def mandar_posicion(message):
-            """al comando /monitor responde con un teclado con el boton para enviar ubicacion"""
-            cid = message.chat.id
-            bot.send_message(cid, "Reportar mi ubicacion", reply_markup=geo)
-
-        @bot.message_handler(content_types=['location',])
-        def recibir_posicion(message):
-            user=message.from_user
-            if user.username:
-                username = user.username
-            elif user.first_name:
-                username = user.first_name
-            elif user.last_name:
-                username = user.last_name
-            latitude = message.location.latitude
-            longitude = message.location.longitude
-            try:
-                Ubicacion.objects.create(lat=latitude, longi=longitude, usuario=username)
-            except Exception:
-                print("falla lat:{}, long:{}, user:{}".format(latitude, longitude, username))
-            # TODO FWD
-
-        bot.polling()
+        medios = MediosBot()
+        medios.bot.polling()
