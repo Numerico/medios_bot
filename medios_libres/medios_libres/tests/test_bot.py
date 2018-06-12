8babeb82953eb0aecdc522cdaff24146c3cb56d3
Roberto Soto <webmaster@numerica.cl>
2018-06-11 21:53:20 -0700

    bot cannot be a class, rewrite as a module (message_handler decoration wasn't getting access to bot instance)

--- medios_libres/medios_libres/tests/test_bot.py
+++ medios_libres/medios_libres/tests/test_bot.py
@@ -1,18 +1,25 @@
 import pytest
-import telebot
 from telebot import types
-from medios_libres.models import MediosBot, WpPosts, WpUsers
-import time
+from medios_libres.models import WpPosts, WpUsers
+import time # TODO needs a litte
+from medios_libres.bot import medios_bot
 
 class TestMediosBot:
 
     @pytest.mark.django_db(transaction=True)
     @pytest.mark.django_use_model(model=[WpPosts, WpUsers])
-    def test_receive_text_message(self):
-        medios = MediosBot('')
+    def test_no_receive_text(self):
+        cmd = self.create_text_message('solo algo')
+        medios_bot.process_new_messages([cmd])
+        time.sleep(0.1)
+        assert WpPosts.objects.count() == 0
+
+    @pytest.mark.django_db(transaction=True)
+    @pytest.mark.django_use_model(model=[WpPosts, WpUsers])
+    def test_receive_command(self):
         cmd = self.create_text_message('/publicar algo')
-        medios.bot.process_new_messages([cmd])
-        time.sleep(0.1) # TODO needs a litte
+        medios_bot.process_new_messages([cmd])
+        time.sleep(0.1)
         assert WpPosts.objects.count() == 1
 
     # HELPERS
